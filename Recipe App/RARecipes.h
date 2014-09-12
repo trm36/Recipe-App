aeeafe8e465aad6d62d66334b3521cfa703baaa0
Taylor Mott <trm36@humboldt.edu>
2014-09-11 20:56:01 -0600

    STEP 3: Import the RARecipes Model into your app
    - Create a new subclass of NSObject called RARecipes in your app.
    - Copy the code from this Gist: https://gist.github.com/jkhowland/5e7c356c73f617bcfdd0
    
    Signed-off-by: Taylor Mott <trm36@humboldt.edu>

---
+++ Recipe App/RARecipes.h
@@ -0,0 +1,22 @@
+//
+//  RARecipes.h
+//  Recipe App
+//
+//  Created by Taylor Mott on 9.11.14.
+//  Copyright (c) 2014 DevMountain. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RARecipes : NSObject
+
++ (NSInteger)count;
++ (NSDictionary *)recipeAtIndex:(NSInteger)index;
++ (NSString *)titleAtIndex:(NSInteger)index;
++ (NSString *)descriptionAtIndex:(NSInteger)index;
++ (NSInteger)ingredientCountAtIndex:(NSInteger)index;
++ (NSString *)ingredientTypeAtIndex:(NSInteger)ingIndex inRecipeAtIndex:(NSInteger)recIndex;
++ (NSString *)ingredientVolumeAtIndex:(NSInteger)ingIndex inRecipeAtIndex:(NSInteger)recIndex;
++ (NSArray *)directions;
+
+@end
